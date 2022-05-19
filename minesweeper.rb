e0171656f4b3605031a85bce49c20a8b6254a7e7
Paul Pomichter <paulmpomichter@gmail.com>
2022-05-18 22:59:15 -0400

    Add user input

--- minesweeper.rb
+++ minesweeper.rb
@@ -6,13 +6,57 @@ class Minesweeper
 
   def initialize
     @board = Board.new
+    @board.seed
+  end
+
+  def get_pos
+    puts "Please enter a position (like 1,2)"
+    print ">"
+
+    pos = self.parse_pos(gets.chomp)
+  end
+
+  def parse_pos(pos)
+    pos.split(",").map(&:to_i)
+  end
+
+  def get_action
+    puts "Please enter f for flag, or r for reveal"
+    print ">"
+
+    action = self.parse_action(gets.chomp)
+  end
+
+  def parse_action(action)
+    action.downcase
+  end
+
+  def play_turn
+    system("clear")
+    @board.render
+
+    pos = self.get_pos
+    action = self.get_action
+
+    if action == "f"
+      @board.place_flag(pos)
+    elsif action == "r"
+      @board.reveal(pos)
+    end
+  end
+
+  def run
+    self.play_turn until @board.over?
+
+    if @board.win?
+      puts "You win!"
+    elsif @board.lose?
+      puts "BOOM!"
+    end
   end
 end
 
 if __FILE__ == $PROGRAM_NAME
   game = Minesweeper.new
-  game.board.seed
-  #testing
-  game.board.place_flag([0, 0])
-  game.board.render
+  game.run
 end
